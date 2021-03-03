a2a15814c8422fb0b66b8ed1e3c0d98a9d6edc04
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-03 20:16:57 +0300

    Добавьте файлы проекта.

---
+++ MyStudyApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Jane";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+
+    }
+       
+}
