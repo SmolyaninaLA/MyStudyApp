fb7043c5c74afff2f9ac69611b88a7ecc8d28ccb
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-03 21:27:13 +0300

    перечисления

--- MyStudyApp/Program.cs
+++ MyStudyApp/Program.cs
@@ -8,21 +8,41 @@ class MainClass
         myName = "Jane";
 
         Console.WriteLine(myName);
-
-        Console.WriteLine("\t Привет, мир");
-
+        Console.WriteLine("\t Привет, \n мир");
         Console.WriteLine("\t Мне 27 лет");
-
         Console.WriteLine("\t Меня зовут \n  Jane");
 
         Console.WriteLine("\u0040");
-
         Console.WriteLine("\u0023");
 
         Console.WriteLine(52);
 
+        DayOfWeek myFavoriteDay;
+        myFavoriteDay = DayOfWeek.Sunday;
+
+        Console.WriteLine("Мой любимфй день недели " + myFavoriteDay);
+
         Console.ReadKey();
 
     }
-       
+
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    enum Semafore : int
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
+     
+    }
+
+
 }
