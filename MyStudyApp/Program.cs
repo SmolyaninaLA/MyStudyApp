b0a51d59bfcc5cdb5ed4b4a135f00bc52a58c19f
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-03 20:26:51 +0300

    Форматировние текста

--- MyStudyApp/Program.cs
+++ MyStudyApp/Program.cs
@@ -9,6 +9,13 @@ class MainClass
 
         Console.WriteLine(myName);
 
+        Console.WriteLine("\t Привет, мир");
+
+        Console.WriteLine("\t Мне 27 лет");
+
+        Console.WriteLine("\t Меня зовут \n  Jane");
+
+
         Console.ReadKey();
 
     }
