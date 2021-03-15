a570767a0238cc893684d6354d099a629adc4e06
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-15 20:35:55 +0300

    Задание 4.3.14

--- MyStudyApp/Program.cs
+++ MyStudyApp/Program.cs
@@ -6,44 +6,21 @@ class MainClass
 
     public static void Main(string[] args)
     {
-       
-        int[] array = { 5,6,9,1,2,3,4 };
-        int   eMax;
-        int SumEl = 0;
 
-        Console.Write("Исходный массив:  ");
+        int[][] array = new int[3][];
 
-        for (int i = 0; i < array.Length; i++)
+        array[0] = new int[2] { 1, 2 };
+        array[1] = new int[3] { 1, 2,3 };
+        array[2] = new int[5] { 1, 2,3 ,4,5};
+
+        foreach (var row in array)
         {
-         
-            Console.Write(array[i] + " ");
-        }
-        Console.WriteLine();
-        Console.Write("Сортировка методом пузырька: ");
-        
-        for (int i = 0; i < array.Length - 1; i++)
-        {
-            for (int j = 0; j < array.Length - i -1; j++  )  
-
-             if (array[j] > array[j+1] )
-                {
-                    eMax = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = eMax;
-                }
+            foreach (var column in row)
+            {
+                Console.Write(column + " ");
+            }
            
         }
-
-            
-
-        for (int i = 0; i < array.Length; i++)
-        {
-            SumEl = SumEl + array[i];
-            Console.Write(array[i] + " ");
-        }
-        Console.WriteLine();
-        Console.WriteLine("Сумма элементов = {0}",SumEl);    
-
-            Console.ReadKey();
+        Console.ReadKey();
     }
 }
