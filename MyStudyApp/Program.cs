a2c7209076e6dfea437735a8c5fdb1d98865aeed
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-15 19:02:22 +0300

    Задание 4.3.7

--- MyStudyApp/Program.cs
+++ MyStudyApp/Program.cs
@@ -2,40 +2,25 @@
 
 class MainClass
 {
+    private const string V = " ";
+
     public static void Main(string[] args)
     {
+        Console.WriteLine("Введите свое имя");
 
-		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-		var color = Console.ReadLine();
-
-		if (color == "red")
-		{
-			Console.BackgroundColor = ConsoleColor.Red;
-			Console.ForegroundColor = ConsoleColor.Black;
-
-			Console.WriteLine("Your color is red!");
-		}
-
-		else if (color == "green")
-		{
-			Console.BackgroundColor = ConsoleColor.Green;
-			Console.ForegroundColor = ConsoleColor.Black;
-
-			Console.WriteLine("Your color is green!");
-		}
-		else
-		{
-			Console.BackgroundColor = ConsoleColor.Cyan;
-			Console.ForegroundColor = ConsoleColor.Black;
-
-			Console.WriteLine("Your color is cyan!");
-		}
-
-		Console.ReadKey();
+        var Name = Console.ReadLine().ToString();
+        string NewName = "";
+   
+        
+      
+        for (int i = Name.Length - 1 ; i >= 0 ; i--)
+        {
+            NewName  = NewName + Name[i] + " ";
+                      
+        }
+        Console.WriteLine("Ваше имя по букваи:");
+        Console.WriteLine("{0} Последняя буква вашего имени : {1}", NewName, Name[Name.Length - 1]);
+        Console.WriteLine("Первая буква имени :{0}",Name[0] );
+        Console.ReadKey();
     }
-
- 
- 
-
 }
