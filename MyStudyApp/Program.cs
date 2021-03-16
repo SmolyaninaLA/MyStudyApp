06a2a5c8ab53e5ada5c4aa4fc48c47718e583144
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-16 22:13:35 +0300

    Задание 4.4.5

--- MyStudyApp/Program.cs
+++ MyStudyApp/Program.cs
@@ -7,19 +7,25 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        var ( name,  age) = ("Евгения",27);
-        Console.WriteLine("Имя {0}", name);
-        Console.WriteLine("Возраст {0}",age);
-
-        Console.WriteLine("Ведите имя :");
-        name = Console.ReadLine();
-
-        Console.WriteLine("Введитн возраст цифрами :");
-        age = Convert.ToInt32( Console.ReadLine());
+        (string Name, string Type, double age, int NameCount) Pet;
 
 
-        Console.WriteLine("Ваше имя {0}",name);
-        Console.WriteLine("Ваш возраст {0}",age);
+        Console.WriteLine("Ведите имя питомца :");
+        Pet.Name = Console.ReadLine();
+
+        Console.WriteLine("Введитr тип питомца :");
+        Pet.Type = Console.ReadLine();
+
+        Console.WriteLine("Введитr возраст питомца :");
+       Pet.age = Convert.ToDouble( Console.ReadLine());
+
+
+        Console.WriteLine(" У вас есть {0} по имени {1}  {2}-ти лет",Pet.Type,Pet.Name,Pet.age);
+
+        Pet.NameCount = Pet.Name.Length;
+
+        Console.WriteLine(" длина имени {0} символов",Pet.NameCount );
+
 
 
 
