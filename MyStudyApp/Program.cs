1b6edff043e7ba8486a7caefba7f98828e7cdac5
Smolyaninala <Smolyaninala@arkona36.ru>
2021-03-16 08:39:52 +0300

    Задание 4.3.17

--- MyStudyApp/Program.cs
+++ MyStudyApp/Program.cs
@@ -8,31 +8,38 @@ class MainClass
     {
 
        int[,] array = { {-5, 6,9,1,2,-3 },{ -8,8,1,1,2,-3} };
-        int countP = 0;
-// через for
-        for(int i = 0; i < array.GetUpperBound(0) + 1 ; i++)
+        int per;
+
+        for(int i = 0; i < array.GetUpperBound(0) + 1; i++)
         {
-            for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
-            {
-                if (array[i, j] > 0)
+            for (int j = 0; j < array.GetUpperBound(1) ; j++)
+            
+                for (int k = j + 1; k < array.GetUpperBound(1) + 1; k++)
                 {
-                    countP++;
+                    if (array[i,j] > array[i,k]) 
+                    {
+                        per = array[i,k];
+                        array[i,k] = array[i,j];
+                        array[i,j] = per;
+                    }
+
                 }
-            }
+
+            
+             
                   
          }
 
-        int countPlus = 0;
-// через foreach
-        foreach(var arr in  array)
+        for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
         {
-            if (arr > 0)
-                countPlus++;
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {
+                Console.Write(array[k,i] + " ");
+            }
+            Console.WriteLine();
+        } 
 
-        }
-
-
-        Console.WriteLine("Количество положительных чисел : {0} {1}",countP,countPlus);
+            
 
         Console.ReadKey();
     }
